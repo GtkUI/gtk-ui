5c0d37810a8939db52dcc0a66013bd2bb931f17a
Zaedus <nintendozaedus@gmail.com>
2022-07-05 20:39:49 -0500

    Refactor the lexer to an OOP approach

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,7 @@
 macro_rules! name_range{() => {'a'..='z' | 'A'..='Z' | '-' | '_'}}
 
+// Tokens
+
 #[derive(Debug)]
 pub enum Definition {
     InlineProp,
@@ -43,220 +45,238 @@ pub enum Token {
     ArgListDeliminator,         // ,
 }
 
-pub fn token_to_string(token: &Token) -> &str {
-    match token {
-        Token::String(_) => "string",
-        Token::Number(_) => "number",
-        Token::Bool(_) => "boolean",
-        Token::Definition(_) => "definition",
-        Token::Directive(_) => "directive",
-        Token::Setter(_) => "setter",
-        Token::Identifier(_) => "identifier",
-        Token::StartBlock => "{",
-        Token::EndBlock => "}",
-        Token::StartArgList => "(",
-        Token::EndArgList => ")",
-        Token::ArgListDeliminator => ","
+impl Token {
+    pub fn to_string(&self) -> &str {
+        match self {
+            Token::String(_) => "string",
+            Token::Number(_) => "number",
+            Token::Bool(_) => "boolean",
+            Token::Definition(_) => "definition",
+            Token::Directive(_) => "directive",
+            Token::Setter(_) => "setter",
+            Token::Identifier(_) => "identifier",
+            Token::StartBlock => "{",
+            Token::EndBlock => "}",
+            Token::StartArgList => "(",
+            Token::EndArgList => ")",
+            Token::ArgListDeliminator => ","
+        }
     }
 }
 
-fn string_to_definition(definition: &str) -> Token {
-    Token::Definition(
-        if definition == "InlineProp" {
-            Definition::InlineProp
-        } else if definition == "InlineArg" {
-            Definition::InlineArg
-        } else if definition == "ChildProp" {
-            Definition::ChildProp
-        } else if definition == "ChildArg" {
-            Definition::ChildArg
+impl Definition {
+    pub fn from(definition: &String) -> Token {
+        Token::Definition(
+            if definition == "InlineProp" {
+                Definition::InlineProp
+            } else if definition == "InlineArg" {
+                Definition::InlineArg
+            } else if definition == "ChildProp" {
+                Definition::ChildProp
+            } else if definition == "ChildArg" {
+                Definition::ChildArg
+            } else {
+                Definition::Object(String::from(definition))
+            }
+        )
+    }
+}
+
+impl Directive {
+    pub fn from(directive: &String) -> Token {
+        let directive_type: Option<Directive>;
+
+        if directive == "include" {
+            directive_type = Some(Directive::Include);
+        } else if directive == "header" {
+            directive_type = Some(Directive::Header);
         } else {
-            Definition::Object(String::from(definition))
+            directive_type = None;
         }
-    )
-}
 
-fn string_to_directive(directive: &str) -> Result<Token, &'static str> {
-    let directive_type: Option<Directive>;
-
-    if directive == "include" {
-        directive_type = Some(Directive::Include);
-    } else if directive == "header" {
-        directive_type = Some(Directive::Header);
-    } else {
-        directive_type = None;
-    }
-
-    match directive_type {
-        Some(t) => Ok(Token::Directive(t)),
-        None => Err("invalid directive")
-    }
-}
-
-fn lex_definition(input: &String, index: &mut usize) -> Token {
-    let mut definition = String::new();
-    *index += 1;
-    for c in input[*index..].chars() {
-        match c {
-            name_range!() => {
-                definition.push(c);
-                *index += 1;
-            },
-            _ => break
+        match directive_type {
+            Some(t) => Token::Directive(t),
+            None => panic!("invalid directive")
         }
     }
-
-    string_to_definition(&definition)
 }
 
-fn lex_directive(input: &String, index: &mut usize) -> Token {
-    let mut directive = String::new();
-    *index += 1;
-    for c in input[*index..].chars() {
-        match c {
-            name_range!() => {
-                directive.push(c);
-                *index += 1;
-            },
-            _ => break
-        }
-    }
+// Lexer
 
-    let parsed_directive = string_to_directive(&directive);
-    
-    match parsed_directive {
-        Ok(k) => k,
-        Err(m) => panic!("{m} '{directive}'")
-    }
+pub struct Lexer<'a> {
+    tokens: Vec<Token>,
+    index: usize,
+    input: &'a String
 }
 
-fn lex_string(input: &String, index: &mut usize) -> Token {
-    let mut string = String::new();
-    for c in input[(*index + 1)..].chars() {
-        match c {
-            '"' => {
-                *index += 2;
-                break
-            },
-            '\n' => {
-                panic!("unexpected end of string");
-            },
-            _ => {
-                *index += 1;
-                string.push(c);
+impl<'a> Lexer<'a> {
+    // Lexing Functions
+    fn definition(&mut self) -> Token {
+        let mut definition = String::new();
+        self.index += 1;
+        for c in self.input[self.index..].chars() {
+            match c {
+                name_range!() => {
+                    definition.push(c);
+                    self.index += 1;
+                },
+                _ => break
             }
         }
+
+        Definition::from(&definition)
     }
 
-    Token::String(string)
-}
-
-fn lex_number(input: &String, index: &mut usize) -> Token {
-    let mut number = String::new();
-    for c in input[*index..].chars() {
-        if ! c.is_digit(10) {
-            break
-        }
-        number.push(c);
-        *index += 1;
-    }
-
-    match number.parse::<i32>() {
-        Ok(num) => Token::Number(num),
-        Err(e) => panic!("{e}")
-    }
-}
-
-
-// TODO: RENAME THIS FUNCTION. Parsing booleans... in the identifier function. Gotta change that 
-fn lex_identifier(input: &String, index: &mut usize) -> Token {
-    let mut identifier = String::new();
-    for c in input[*index..].chars() {
-        match c {
-            name_range!() => {
-                *index += 1;
-                identifier.push(c);
-            },
-            _ => break
-        }
-    }
-
-    match identifier.as_str() {
-        "true"   => Token::Bool(1),
-        "false"  => Token::Bool(0),
-        "String" => Token::Identifier(Identifier::Type(TypeIdentifier::String)),
-        "Number" => Token::Identifier(Identifier::Type(TypeIdentifier::Number)),
-        "Bool"   => Token::Identifier(Identifier::Type(TypeIdentifier::Bool)),
-        _        => Token::Identifier(Identifier::Generic(identifier))
-    }
-}
-
-fn lex_setter(input: &String, index: &mut usize) -> Token {
-    let mut setter = String::new();
-    *index += 1;
-    for c in input[*index..].chars() {
-        match c {
-            name_range!() => {
-                setter.push(c);
-                *index += 1;
-            },
-            _ => break
-        }
-    }
-
-    Token::Setter(setter)
-}
-
-fn process_comment(input: &String, index: &mut usize) -> bool {
-    for c in input[*index..].chars() {
-        *index += 1;
-        if c == '\n' {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-fn add_and_move(token: Token, index: &mut usize) -> Token {
-    *index += 1;
-    token
-}
-
-pub fn lex(input: &String) -> Vec<Token> {
-    let mut tokens: Vec<Token> = Vec::new();
-    let mut index = 0;
-    loop {
-        let input_char = input.chars().nth(index);
-        if let Some(c) = input_char {
-            tokens.push(match c {
-                '@'           => lex_definition(&input, &mut index),
-                '#'           => lex_directive(&input, &mut index),
-                '"'           => lex_string(&input, &mut index),
-                '.'           => lex_setter(&input, &mut index),
-                '0'..='9'     => lex_number(&input, &mut index),
-                name_range!() => lex_identifier(&input, &mut index),
-                '{'           => add_and_move(Token::StartBlock, &mut index),
-                '}'           => add_and_move(Token::EndBlock, &mut index),
-                ','           => add_and_move(Token::ArgListDeliminator, &mut index),
-                '('           => add_and_move(Token::StartArgList, &mut index),
-                ')'           => add_and_move(Token::EndArgList, &mut index),
-                ' ' | '\n'    => {
-                    index += 1;
-                    continue
+    fn directive(&mut self) -> Token {
+        let mut directive = String::new();
+        self.index += 1;
+        for c in self.input[self.index..].chars() {
+            match c {
+                name_range!() => {
+                    directive.push(c);
+                    self.index += 1;
                 },
-                '/' => {
-                    if process_comment(&input, &mut index) {
+                _ => break
+            }
+        }
+        
+        Directive::from(&directive)
+    }
+
+    fn string(&mut self) -> Token {
+        let mut string = String::new();
+        for c in self.input[(self.index + 1)..].chars() {
+            match c {
+                '"' => {
+                    self.index += 2;
+                    break
+                },
+                '\n' => {
+                    panic!("unexpected end of string");
+                },
+                _ => {
+                    self.index += 1;
+                    string.push(c);
+                }
+            }
+        }
+
+        Token::String(string)
+    }
+
+    fn setter(&mut self) -> Token {
+        let mut setter = String::new();
+        self.index += 1;
+        for c in self.input[self.index..].chars() {
+            match c {
+                name_range!() => {
+                    setter.push(c);
+                    self.index += 1;
+                },
+                _ => break
+            }
+        }
+
+        Token::Setter(setter)
+    }
+
+    fn number(&mut self) -> Token {
+        let mut number = String::new();
+        for c in self.input[self.index..].chars() {
+            if ! c.is_digit(10) {
+                break
+            }
+            number.push(c);
+            self.index += 1;
+        }
+
+        match number.parse::<i32>() {
+            Ok(num) => Token::Number(num),
+            Err(e) => panic!("{e}")
+        }
+    }
+
+    fn identifier(&mut self) -> Token {
+        let mut identifier = String::new();
+        for c in self.input[self.index..].chars() {
+            match c {
+                name_range!() => {
+                    self.index += 1;
+                    identifier.push(c);
+                },
+                _ => break
+            }
+        }
+
+        match identifier.as_str() {
+            "true"   => Token::Bool(1),
+            "false"  => Token::Bool(0),
+            "String" => Token::Identifier(Identifier::Type(TypeIdentifier::String)),
+            "Number" => Token::Identifier(Identifier::Type(TypeIdentifier::Number)),
+            "Bool"   => Token::Identifier(Identifier::Type(TypeIdentifier::Bool)),
+            _        => Token::Identifier(Identifier::Generic(identifier))
+        }
+    }
+
+    fn add_and_move(&mut self, token: Token) -> Token {
+        self.index += 1;
+        token
+    }
+
+    fn comment(&mut self) -> bool {
+        for c in self.input[self.index..].chars() {
+            self.index += 1;
+            if c == '\n' {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Pubs
+    pub fn new(s: &'a String) -> Lexer<'a> {
+        return Lexer {
+            tokens: Vec::new(),
+            index: 0,
+            input: s, 
+        }
+    }
+    pub fn lex(&mut self) -> &Vec<Token> {
+        
+        loop {
+            let input_char = self.input.chars().nth(self.index);
+            if let Some(c) = input_char {
+                let token = match c {
+                    '@'           => self.definition(),
+                    '#'           => self.directive(),
+                    '"'           => self.string(),
+                    '.'           => self.setter(),
+                    '0'..='9'     => self.number(),
+                    name_range!() => self.identifier(),
+                    '{'           => self.add_and_move(Token::StartBlock),
+                    '}'           => self.add_and_move(Token::EndBlock),
+                    ','           => self.add_and_move(Token::ArgListDeliminator),
+                    '('           => self.add_and_move(Token::StartArgList),
+                    ')'           => self.add_and_move(Token::EndArgList),
+                    ' ' | '\n'    => {
+                        self.index += 1;
                         continue
-                    } else {
-                        panic!("Unexpected token '/'");
-                    }
-                },
-                _ => Token::Number(-1),
-            })
-        } else {
-            break;
-        }
+                    },
+                    '/' => {
+                        if self.comment() {
+                            continue
+                        } else {
+                            panic!("Unexpected token '/'");
+                        }
+                    },
+                    _ => Token::Number(-1),
+                };
+                self.tokens.push(token);
+            } else {
+                break;
+            }
     }
-    tokens
+        &self.tokens
+    }
 }
