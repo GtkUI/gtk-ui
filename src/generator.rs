fdd44d967c3f801e44d9e143dbf4b8d128453dfd
Zaedus <nintendozaedus@gmail.com>
2022-08-20 23:40:48 -0500

    Add error handling in generator

--- src/generator.rs
+++ src/generator.rs
@@ -7,7 +7,8 @@ use super::lexer::{
     DefinitionType as TokenDefinitionType,
     TypeIdentifierType as TokenTypeIdentifierType,
     Token,
-    TokenValue
+    TokenValue,
+    Position
 };
 use std::fs::File;
 use std::io::Write;
@@ -34,22 +35,22 @@ pub struct Generator {
 
 impl Generator {
 
-    fn is_valid_type(token: &Token, expected_type: &TokenTypeIdentifierType) -> bool {
+    fn is_valid_type(token: &Token, expected_type: &TokenTypeIdentifierType) -> Result<bool, (String, Position)> {
         match token.value {
             TokenValue::Bool(_) => {
-                matches!(expected_type, TokenTypeIdentifierType::Bool)
+                Ok(matches!(expected_type, TokenTypeIdentifierType::Bool))
             },
             TokenValue::Number(_) => {
-                matches!(expected_type, TokenTypeIdentifierType::Number)
+                Ok(matches!(expected_type, TokenTypeIdentifierType::Number))
             },
             TokenValue::String(_) => {
-                matches!(expected_type, TokenTypeIdentifierType::String)
+                Ok(matches!(expected_type, TokenTypeIdentifierType::String))
             },
-            _ => panic!("{} is not a primitive and therefore it's type cannot be checked", token.to_string())
+            _ => Err((format!("{} is not a primitive and therefore it's type cannot be checked", token.to_string()), token.position))
         }
     }
 
-    pub fn generate_from_collective(&self, children: &Vec<Statement>) -> String {
+    pub fn generate_from_collective(&self, children: &Vec<Statement>) -> Result<String, (String, Position)> {
         let mut result = String::new();
 
         for child in children {
@@ -59,7 +60,7 @@ impl Generator {
                         match definition {
                             CachedDefinition::Raw(definition) => {
                                 if definition.args.len() != object.arguments.len() {
-                                    panic!("the '{}' definition expects {} args, {} given", object.name, definition.args.len(), object.arguments.len());
+                                    return Err((format!("the '{}' definition expects {} args, {} given", object.name, definition.args.len(), object.arguments.len()), child.position));
                                 }
 
                                 let mut inlines: Vec<(String, String)> = Vec::new();
@@ -70,15 +71,17 @@ impl Generator {
                                     let actual_arg = &object.arguments[i];
                                     
                                     // Check if actual and defined are the same type and if so check if the definition specifies it as an inline or a child
-                                    if Generator::is_valid_type(&actual_arg, &defined_arg.1) {
-                                        match defined_arg.2 {
-                                            TokenDefinitionType::InlineArg => {
-                                                inlines.push((defined_arg.0.clone(), actual_arg.value_to_string()));
-                                            },
-                                            TokenDefinitionType::ChildArg => {
-                                                children.push((defined_arg.0.clone(), actual_arg.value_to_string()));
-                                            },
-                                            _ => panic!("expected either an InlineArg or a ChildArg, got {}", defined_arg.2.to_string())
+                                    if let Ok(is_valid) = Generator::is_valid_type(&actual_arg, &defined_arg.1) {
+                                        if is_valid {
+                                            match defined_arg.2 {
+                                                TokenDefinitionType::InlineArg => {
+                                                    inlines.push((defined_arg.0.clone(), actual_arg.value_to_string()));
+                                                },
+                                                TokenDefinitionType::ChildArg => {
+                                                    children.push((defined_arg.0.clone(), actual_arg.value_to_string()));
+                                                },
+                                                _ => return Err((format!("expected either an InlineArg or a ChildArg, got {}", defined_arg.2.to_string()), actual_arg.position))
+                                            }
                                         }
                                     }
                                 }
@@ -89,15 +92,17 @@ impl Generator {
 
                                     if let Some(defined_prop) = defined_prop {
                                         // Check if actual and defined are the same type and if so check if the definition specifies it as an inline or a child
-                                        if Generator::is_valid_type(&actual_prop, &defined_prop.0) {
-                                            match defined_prop.1 {
-                                                TokenDefinitionType::InlineProp => {
-                                                    inlines.push((setter.name.clone(), actual_prop.value_to_string()));
-                                                },
-                                                TokenDefinitionType::ChildProp => {
-                                                    children.push((setter.name.clone(), actual_prop.value_to_string()));
-                                                },
-                                                _ => panic!("expected either an InlineArg or a ChildArg, got {}", defined_prop.1.to_string())
+                                        if let Ok(is_valid) = Generator::is_valid_type(&actual_prop, &defined_prop.0) {
+                                            if is_valid {
+                                                match defined_prop.1 {
+                                                    TokenDefinitionType::InlineProp => {
+                                                        inlines.push((setter.name.clone(), actual_prop.value_to_string()));
+                                                    },
+                                                    TokenDefinitionType::ChildProp => {
+                                                        children.push((setter.name.clone(), actual_prop.value_to_string()));
+                                                    },
+                                                    _ => return Err((format!("expected either an InlineArg or a ChildArg, got {}", defined_prop.1.to_string()), actual_prop.position))
+                                                }
                                             }
                                         }
                                     }
@@ -130,7 +135,12 @@ impl Generator {
 
                                 if object.children.len() > 0 {
                                     result += "<child>\n";
-                                    result += self.generate_from_collective(&object.children).as_str();
+                                    match self.generate_from_collective(&object.children) {
+                                        Ok(collective) => {
+                                            result += collective.as_str();
+                                        },
+                                        Err(err) => return Err(err)
+                                    }
                                     result += "</child>\n";
                                 }
                                 result += "</object>\n"
@@ -142,15 +152,15 @@ impl Generator {
 
                     }
                 },
-                _ => panic!("found {}, expected object in collective definition", child.to_string())
+                _ => return Err((format!("found {}, expected object in collective definition", child.to_string()), child.position))
             }
         }
         
 
-        result
+        Ok(result)
     }
 
-    pub fn generate_from_raw(properties: &Vec<Statement>) -> CachedRawDefinition {
+    pub fn generate_from_raw(properties: &Vec<Statement>) -> Result<CachedRawDefinition, (String, Position)> {
         let mut props: HashMap<String, (TokenTypeIdentifierType, TokenDefinitionType)> = HashMap::new();
         let mut args: Vec<(String, TokenTypeIdentifierType, TokenDefinitionType)> = Vec::new();
 
@@ -163,18 +173,18 @@ impl Generator {
                     TokenDefinitionType::InlineArg | TokenDefinitionType::ChildArg => {
                         args.push((property_value.name.clone(), property_value.internal_type.clone(), property_value.definition_type.clone()));
                     },
-                    _ => panic!("expected a property definition, found {}", property_value.definition_type.to_string())
+                    _ => return Err((format!("expected a property definition, found {}", property_value.definition_type.to_string()), property.position))
                 }
             }
         }
 
-        CachedRawDefinition {
+        Ok(CachedRawDefinition {
             props, args
-        }
+        })
     }
     
     // Pubs
-    pub fn generate(&mut self) {
+    pub fn generate(&mut self) -> Result<(), (String, Position)> {
         for statement in &self.statements {
             match &statement.value {
                 StatementValue::Definition(definition) => {
@@ -182,7 +192,12 @@ impl Generator {
                         DefinitionType::Root(filename) => {
                             print!("Writing {}.ui... ", filename);
                             let mut file_content = String::new();
-                            file_content.push_str(self.generate_from_collective(&definition.children).as_str());
+                            match self.generate_from_collective(&definition.children) {
+                                Ok(collective) => {
+                                    file_content.push_str(collective.as_str());
+                                },
+                                Err(err) => return Err(err)
+                            }
                             file_content.push_str("</interface>");
 
                             let mut file = File::create(format!("{}.ui", filename)).expect("failed to create output file");
@@ -191,10 +206,20 @@ impl Generator {
                             println!("done!");
                         },
                         DefinitionType::Collective => {
-                            self.definitions.insert(definition.name.clone(), CachedDefinition::Collective(self.generate_from_collective(&definition.children)));
+                            match self.generate_from_collective(&definition.children) {
+                                Ok(collective) => {
+                                    self.definitions.insert(definition.name.clone(), CachedDefinition::Collective(collective));
+                                },
+                                Err(err) => return Err(err)
+                            }
                         },
                         DefinitionType::Raw => {
-                            self.definitions.insert(definition.name.clone(), CachedDefinition::Raw(Generator::generate_from_raw(&definition.children)));
+                            match Generator::generate_from_raw(&definition.children) {
+                                Ok(raw) => {
+                                    self.definitions.insert(definition.name.clone(), CachedDefinition::Raw(raw));
+                                },
+                                Err(err) => return Err(err)
+                            }
                         }
                     }
                 },
@@ -202,9 +227,10 @@ impl Generator {
                     self.header.push_str(header);
                     self.header.push('\n');
                 },
-                _ => panic!("this should never ever ever ever ever happen. something must be wrong with the parser if this does happen")
+                _ => return Err((format!("this should never ever ever ever ever happen. something must be wrong with the parser if this does happen"), statement.position))
             }
         }
+        Ok(())
     }
 
     pub fn new(statements: Vec<Statement>) -> Self {
